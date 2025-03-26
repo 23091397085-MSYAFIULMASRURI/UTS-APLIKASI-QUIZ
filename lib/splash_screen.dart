c1eeab1d791d1641ec076efc7408d2eee09aab27
Msyafiulmasruri <m.23085@mhs.unesa.ac.id>
2025-03-26 12:21:08 +0700

    Initial Commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'main.dart'; // Import file utama aplikasi
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Setelah 3 detik, pindah ke halaman utama (Quizzler)
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Quizzler()), // Pindah ke halaman utama
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Background hitam
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo aplikasi
+            Image.asset(
+              'images/logo.png', // Pastikan file logo ada di folder assets
+              width: 250,
+              height: 250,
+            ),
+            SizedBox(height: 20),
+            // Animasi loading
+            CircularProgressIndicator(
+              color: Colors.white, // Warna loading putih
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
